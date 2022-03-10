a688acf0474e14cf80303754eb35bcc597bebf60
KellyPark96 <kellypark2032@gmail.com>
2022-03-10 19:19:56 +0900

    useState() remove comment

--- src/App.js
+++ src/App.js
@@ -7,23 +7,13 @@ function App() {
 	const [keyword, setKeyword] = useState("");
 	const onClick = () => setValue((prev) => prev + 1);
 	const onChange = (event) => setKeyword(event.target.value);
-
 	console.log("I run all the time");
-
 	useEffect(() => {
 		console.log("I run only once.");
 	}, []);
-
-	// useEffect(() => {
-	// 	if (keyword !== "" && keyword.length > 5) {
-	// 		console.log("I run when 'keyword' changes", keyword);
-	// 	}
-	// }, [keyword]);
 	useEffect(() => {
 		console.log("I run when 'keywor & counter' changes");
 	}, [keyword, counter]);
-	// 즉, useEffect는 component가 시작되는 시작점이나,
-	// 무언가가 update될때만 코드를 실행할 수 있음.
 
 	return (
 		<div>
