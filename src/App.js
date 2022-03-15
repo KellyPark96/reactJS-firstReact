7dadf3282fa0be7a7e021676d8b9486731014f78
KellyPark96 <kellypark2032@gmail.com>
2022-03-15 23:33:04 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-	const [toDo, setToDo] = useState("");
-	// state를 직접 수정하면 안됨.
-	const [toDos, setToDos] = useState([]);
-	const onChange = (event) => setToDo(event.target.value);
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if (toDo === "") {
-			return;
-		}
-		setToDo("");
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
+	const [cost, setCost] = useState("Select:");
+	const [count, setCount] = useState(1);
+	const onChange = (event) => {
+		setCost(event.target.value);
+		setCount(1);
 	};
-	console.log(toDos);
+	const onInputHandle = (event) => setCount(event.target.value);
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((response) => response.json())
+			.then((json) => {
+				console.log(json.slice(0, 30));
+				setCoins(json);
+				setLoading(false);
+			});
+	}, []);
 	return (
 		<div>
-			<form onSubmit={onSubmit}>
-				<input
-					onChange={onChange}
-					value={toDo}
-					type="text"
-					placeholder="Write your to do..."
-				/>
-				<button>Add To Do</button>
-			</form>
+			<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+			{loading ? (
+				<strong>loading...</strong>
+			) : (
+				<select onChange={onChange}>
+					<option>Select: </option>
+					{coins.map((coin, index) => (
+						<option
+							key={index}
+							value={coin.quotes.USD.price}
+							id={coin.symbol}
+						>
+							{coin.name}({coin.symbol}) : $
+							{coin.quotes.USD.price}
+						</option>
+					))}
+				</select>
+			)}
+			<hr />
+			{cost === "Select:" ? null : (
+				<div>
+					<input
+						type="number"
+						value={count}
+						onChange={onInputHandle}
+					/>
+					<h5>You can buy {Math.floor(count / cost)}</h5>
+				</div>
+			)}
 		</div>
 	);
 }
