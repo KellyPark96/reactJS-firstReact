75144af4bc6061ef5c5ead643781f6fe416cf72b
KellyPark96 <kellypark2032@gmail.com>
2022-03-16 22:16:36 +0900

    getData to fetch / async-await

--- src/App.js
+++ src/App.js
@@ -2,55 +2,46 @@ import { useEffect, useState } from "react";
 
 function App() {
 	const [loading, setLoading] = useState(true);
-	const [coins, setCoins] = useState([]);
-	const [cost, setCost] = useState("Select:");
-	const [count, setCount] = useState(1);
-	const onChange = (event) => {
-		setCost(event.target.value);
-		setCount(1);
+	const [movies, setMovies] = useState([]);
+	const getMovies = async () => {
+		const json = await (
+			await fetch(
+				`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+			)
+		).json();
+		setMovies(json.data.movies);
+		setLoading(false);
 	};
-	const onInputHandle = (event) => setCount(event.target.value);
 	useEffect(() => {
-		fetch("https://api.coinpaprika.com/v1/tickers")
-			.then((response) => response.json())
-			.then((json) => {
-				console.log(json.slice(0, 30));
-				setCoins(json);
-				setLoading(false);
-			});
+		getMovies();
 	}, []);
 	return (
 		<div>
-			<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
 			{loading ? (
-				<strong>loading...</strong>
+				<h1>Loading...</h1>
 			) : (
-				<select onChange={onChange}>
-					<option>Select: </option>
-					{coins.map((coin, index) => (
-						<option
-							key={index}
-							value={coin.quotes.USD.price}
-							id={coin.symbol}
-						>
-							{coin.name}({coin.symbol}) : $
-							{coin.quotes.USD.price}
-						</option>
-					))}
-				</select>
-			)}
-			<hr />
-			{cost === "Select:" ? null : (
 				<div>
-					<input
-						type="number"
-						value={count}
-						onChange={onInputHandle}
-					/>
-					<h5>You can buy {Math.floor(count / cost)}</h5>
+					{movies.map((movie) => (
+						<div key={movie.id}>
+							<img
+								src={movie.medium_cover_image}
+								alt={movie.title}
+							/>
+							<h2>{movie.title}</h2>
+							<p>{movie.summary}</p>
+							{movie.hasOwnProperty("genres") ? (
+								<ul>
+									{movie.genres.map((genre) => (
+										<li key={genre}>{genre}</li>
+									))}
+								</ul>
+							) : null}
+						</div>
+					))}
 				</div>
 			)}
 		</div>
 	);
 }
+
 export default App;
