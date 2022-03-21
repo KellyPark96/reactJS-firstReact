10d4d1e6391a4aa497a1718c7c0a57e0a8fe7404
KellyPark96 <kellypark2032@gmail.com>
2022-03-21 21:47:52 +0900

    connect router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path="/movie" element={<Detail />}></Route>
 				<Route path="/" element={<Home />}></Route>
 			</Routes>
 		</Router>
 	);
 }
-
+// BrowserRouter => #이 없음
+// HashRouter => localhose:3000/# 이 붙음.
 export default App;
